73896a0b2b835a70518fc64dce9f93d60a946ee7
fnozoszzt <fnozoszzt@gmail.com>
2015-11-14 21:53:35 +0800

    update

---
+++ learn.sh
@@ -0,0 +1,31 @@
+# fitst mkdir a new dir and in
+mkdir learngit
+
+# Initialized repository
+git init
+
+
+# create a new file <a>
+# a is empty
+touch a
+
+# add <a> into stage
+# or change <a> ,add ew <a> into stage
+git add a
+
+# status
+git status
+
+# commit
+git commit -m 'log'
+
+# checkout file from stage
+git checkout -- <file>
+
+# unstage
+git reset HEAD file
+
+# 
+git rm <file>
+
+
