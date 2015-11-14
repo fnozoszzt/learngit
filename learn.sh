3e50111b78b9b800e80e1909b680f4747214c105
fnozoszzt <fnozoszzt@gmail.com>
2015-11-15 00:11:17 +0800

    -

--- learn.sh
+++ learn.sh
@@ -31,4 +31,27 @@ git rm <file>
 # 
 git push -u origin master
 
+#
+git remote add origin git@github.com:fnozoszzt/learngit.git
+
+
+git clone git@github.com:fnozoszzt/learngit.git
+
+
+
+# new branch <dev>
+git checkout -b dev
+# equal to 
+git branch dev
+git checkout dev
+
+
+# commit on branch
+git commit -m "branch test"
+
+# switch to master
+git checkout master
+
+#
+git merge dev
 
